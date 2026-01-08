19538d3278b1684df011704f0df8d49744025180
Naduni Madawala <250956343+Naduni-Madawala@users.noreply.github.com>
2026-01-09 05:04:37 +0530

    Update Database.php

--- ratings/html/src/Database.php
+++ ratings/html/src/Database.php
@@ -1,1 +1,18 @@
+<?php
+namespace ToyShop;
+
+use PDO;
+
+class Database {
+    public static function getConnection(): PDO {
+        return new PDO(
+            "mysql:host=mysql;dbname=toyshop",
+            "root",
+            "rootpass",
+            [
+                PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+            ]
+        );
+    }
+}
 
