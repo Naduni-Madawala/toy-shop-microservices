d814332f8334c8ef33918ffedd318d63c471987e
Naduni Madawala <250956343+Naduni-Madawala@users.noreply.github.com>
2026-01-09 05:25:18 +0530

    Update controller.js

--- web/static/js/controller.js
+++ web/static/js/controller.js
@@ -1,31 +1,23 @@
-<!DOCTYPE html>
-<html ng-app="toyShop">
-<head>
-  <title>Toy Shop</title>
-  <link rel="stylesheet" href="css/style.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.8.2/angular.min.js"></script>
-  <script src="js/controller.js"></script>
-</head>
-<body ng-controller="MainCtrl">
+var app = angular.module("ToyShopApp", []);
 
-<h1>🧸 Toy Shop</h1>
+app.controller("MainController", function ($scope, $http) {
 
-<h2>🚗 Model Cars</h2>
-<div class="grid">
-  <div ng-repeat="p in cars">
-    <img ng-src="images/cars/{{p.image}}">
-    <p>{{p.name}} – Rs {{p.price}}</p>
-  </div>
-</div>
+  $scope.products = [];
+  $scope.cart = [];
 
-<h2>🎎 Anime Figures</h2>
-<div class="grid">
-  <div ng-repeat="p in anime">
-    <img ng-src="images/anime/{{p.image}}">
-    <p>{{p.name}} – Rs {{p.price}}</p>
-  </div>
-</div>
+  $scope.loadCatalogue = function () {
+    $http.get("/api/catalogue")
+      .then(function (response) {
+        $scope.products = response.data;
+      });
+  };
 
-</body>
-</html>
+  $scope.addToCart = function (product) {
+    $http.post("/api/cart", product)
+      .then(function () {
+        alert("Added to cart");
+      });
+  };
 
+  $scope.loadCatalogue();
+});
