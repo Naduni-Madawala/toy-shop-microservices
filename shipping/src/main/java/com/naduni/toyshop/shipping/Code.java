3127e75644d81feca6bc3a7932fea88e482f4b78
Naduni Madawala <250956343+Naduni-Madawala@users.noreply.github.com>
2026-01-09 05:17:13 +0530

    Update Code.java

--- shipping/src/main/java/com/naduni/toyshop/shipping/Code.java
+++ shipping/src/main/java/com/naduni/toyshop/shipping/Code.java
@@ -1,1 +1,16 @@
+package com.naduni.toyshop.shipping;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+public class Code {
+
+    @Id
+    private Long id;
+    private String code;
+
+    public Long getId() { return id; }
+    public String getCode() { return code; }
+}
 
