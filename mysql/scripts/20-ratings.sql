07be534fac118cbb2343f1c81e4e58930d5e7e3b
Naduni Madawala <250956343+Naduni-Madawala@users.noreply.github.com>
2026-01-09 04:42:54 +0530

    Update 20-ratings.sql

--- mysql/scripts/20-ratings.sql
+++ mysql/scripts/20-ratings.sql
@@ -1,1 +1,15 @@
+USE toyshop;
+
+CREATE TABLE IF NOT EXISTS ratings (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  product_name VARCHAR(255),
+  rating INT,
+  comment VARCHAR(255)
+);
+
+INSERT INTO ratings (product_name, rating, comment) VALUES
+('Ferrari SF-24 Diecast Model Car 2024 Formula 1', 5, 'Excellent model car'),
+('Steve Harrington Funko Pop', 4, 'Great StrangerThings figure'),
+('MINIATURA CARRO F1 REDBULL MAX VERSTAPPEN RB19', 5, 'High quality'),
+('Stranger Things Demogorgon Pop! Vinyl Figure', 5, 'Very detailed');
 
