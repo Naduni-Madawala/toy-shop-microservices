7494c1805045a8cedfb4a0ada2dbf2eadeb4c8ee
Naduni Madawala <250956343+Naduni-Madawala@users.noreply.github.com>
2026-01-09 04:45:21 +0530

    Update payment.py

--- payment/payment.py
+++ payment/payment.py
@@ -1,13 +1,20 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+from rabbitmq import publish_message
 
 app = Flask(__name__)
 
-@app.route("/pay")
+@app.route("/pay", methods=["POST"])
 def pay():
+    order = request.json or {}
+    publish_message(str(order))
     return jsonify({
         "status": "success",
         "message": "Toy order payment processed"
     })
 
-app.run(host="0.0.0.0", port=8080)
+@app.route("/health")
+def health():
+    return "OK"
 
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080)
