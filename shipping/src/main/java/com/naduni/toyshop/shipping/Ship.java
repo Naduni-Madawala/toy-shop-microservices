af3239ed74949f0f88db8983d7c8e6b648567fd7
Naduni Madawala <250956343+Naduni-Madawala@users.noreply.github.com>
2026-01-09 05:15:56 +0530

    Update Ship.java

--- shipping/src/main/java/com/naduni/toyshop/shipping/Ship.java
+++ shipping/src/main/java/com/naduni/toyshop/shipping/Ship.java
@@ -1,1 +1,20 @@
+package com.naduni.toyshop.shipping;
+
+public class Ship {
+    private String status;
+    private int cost;
+
+    public Ship(String status, int cost) {
+        this.status = status;
+        this.cost = cost;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public int getCost() {
+        return cost;
+    }
+}
 
