c8e98efc931e8a37b09f775bc716d64724e5a088
Naduni Madawala <250956343+Naduni-Madawala@users.noreply.github.com>
2026-01-09 04:44:28 +0530

    Update rabbitmq.py

--- payment/rabbitmq.py
+++ payment/rabbitmq.py
@@ -1,1 +1,25 @@
+import pika
+import configparser
+
+config = configparser.ConfigParser()
+config.read("payment.ini")
+
+RABBIT_HOST = config["rabbitmq"]["host"]
+QUEUE = config["rabbitmq"]["queue"]
+
+def publish_message(message):
+    connection = pika.BlockingConnection(
+        pika.ConnectionParameters(host=RABBIT_HOST)
+    )
+    channel = connection.channel()
+
+    channel.queue_declare(queue=QUEUE)
+
+    channel.basic_publish(
+        exchange="",
+        routing_key=QUEUE,
+        body=message
+    )
+
+    connection.close()
 
