cef3449e4f24c9fee678c139a90e34580674f015
Naduni Madawala <250956343+Naduni-Madawala@users.noreply.github.com>
2026-01-09 04:23:58 +0530

    Update users.js

--- mongo/users.js
+++ mongo/users.js
@@ -1,1 +1,17 @@
+db = db.getSiblingDB("toyshop");
+
+db.users.insertMany([
+  {
+    username: "customer1",
+    name: "Toy Shop Customer",
+    email: "customer1@toyshop.com"
+  },
+  {
+    username: "admin",
+    name: "Toy Shop Admin",
+    email: "admin@toyshop.com"
+  }
+]);
+
+print("✅ Toy Shop users initialized");
 
