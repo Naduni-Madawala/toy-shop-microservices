144b175fd6b816bec7cc4e98e78ee2c1651c465d
Naduni Madawala <250956343+Naduni-Madawala@users.noreply.github.com>
2026-01-09 03:55:36 +0530

    Update build.sh

--- load-gen/build.sh
+++ load-gen/build.sh
@@ -1,1 +1,10 @@
+#!/bin/bash
+set -e
+
+IMAGE_NAME=naduni/toy-loadgen
+
+echo "Building Toy Shop load generator image..."
+docker build -t $IMAGE_NAME .
+
+echo "Load generator image built successfully"
 
