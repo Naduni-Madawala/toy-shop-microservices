38f08a27a6650061b5286acda7e088a4e05880d1
Naduni Madawala <250956343+Naduni-Madawala@users.noreply.github.com>
2026-01-09 03:53:13 +0530

    Update run.sh

--- fluentd/Docker-Compose/run.sh
+++ fluentd/Docker-Compose/run.sh
@@ -1,1 +1,13 @@
+#!/bin/bash
+set -e
+
+echo "Starting Fluentd (Docker Compose mode)..."
+
+docker run -d \
+  --name toy-fluentd \
+  -p 24224:24224 \
+  -p 24224:24224/udp \
+  naduni/toy-fluentd
+
+echo "Fluentd is running on port 24224"
 
