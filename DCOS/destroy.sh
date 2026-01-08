4068c39b8133eb71ec11d9198dd6c2850c741a57
Naduni Madawala <250956343+Naduni-Madawala@users.noreply.github.com>
2026-01-09 03:48:35 +0530

    Update destroy.sh

--- DCOS/destroy.sh
+++ DCOS/destroy.sh
@@ -1,1 +1,13 @@
+#!/bin/bash
+set -e
+
+echo "Removing Toy Shop services from DC/OS..."
+
+for app in manifest/*.json; do
+  APP_ID=$(jq -r '.id' "$app")
+  echo "Removing $APP_ID"
+  dcos marathon app remove "$APP_ID" || true
+done
+
+echo "🧹 Toy Shop removed from DC/OS"
 
