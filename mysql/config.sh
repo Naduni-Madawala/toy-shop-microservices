9c4e17109dff172dfa9993b0109bdf26421e9fab
Naduni Madawala <250956343+Naduni-Madawala@users.noreply.github.com>
2026-01-09 04:40:14 +0530

    Update config.sh

--- mysql/config.sh
+++ mysql/config.sh
@@ -1,1 +1,11 @@
+#!/bin/bash
+set -e
+
+echo "Configuring Toy Shop MySQL database..."
+
+mysql -u root -p$MYSQL_ROOT_PASSWORD <<EOF
+CREATE DATABASE IF NOT EXISTS toyshop;
+EOF
+
+echo "MySQL configuration completed"
 
