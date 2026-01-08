359f1358f0f768e6375c77cc5917fcb6b212aed1
Naduni Madawala <250956343+Naduni-Madawala@users.noreply.github.com>
2026-01-09 02:33:19 +0530

    Update server.js

--- user/server.js
+++ user/server.js
@@ -1,1 +1,13 @@
+const express = require("express");
+const app = express();
+
+app.get("/users", (req, res) => {
+  res.json([
+    { id: 1, name: "ToyShop Customer" }
+  ]);
+});
+
+app.listen(8080, () => {
+  console.log("👤 User service running");
+});
 
