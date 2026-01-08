26f5246d7ad4fceff756e99ee193f570f5223930
Naduni Madawala <250956343+Naduni-Madawala@users.noreply.github.com>
2026-01-09 05:08:55 +0530

    Update CatalogueService.php

--- ratings/html/src/Service/CatalogueService.php
+++ ratings/html/src/Service/CatalogueService.php
@@ -1,1 +1,13 @@
+<?php
+namespace ToyShop\Service;
+
+class CatalogueService {
+    public function getProducts(): array {
+        return [
+            "Ferrari SF-24 Diecast Model Car 2024 Formula 1",
+            "Steve Harrington Funko Pop",
+            "Stranger Things Demogorgon Pop! Vinyl Figure"
+        ];
+    }
+}
 
