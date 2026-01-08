eb7981991ade269be3aa0e69378dd3ecce4137c6
Naduni Madawala <250956343+Naduni-Madawala@users.noreply.github.com>
2026-01-09 03:41:12 +0530

    Update server.js

--- cart/server.js
+++ cart/server.js
@@ -1,20 +1,38 @@
 const express = require("express");
-const app = express();
+const bodyParser = require("body-parser");
 
-app.use(express.json());
+const app = express();
+app.use(bodyParser.json());
 
 let cart = [];
 
+/**
+ * Add item to cart
+ */
 app.post("/cart", (req, res) => {
-  cart.push(req.body);
-  res.json({ message: "Item added to toy cart", cart });
+  const item = req.body;
+  cart.push(item);
+  res.json({
+    message: "Item added to toy cart",
+    cart
+  });
 });
 
+/**
+ * Get cart
+ */
 app.get("/cart", (req, res) => {
   res.json(cart);
 });
 
-app.listen(8080, () => {
-  console.log("🛒 Cart service running");
+/**
+ * Clear cart
+ */
+app.delete("/cart", (req, res) => {
+  cart = [];
+  res.json({ message: "Toy cart cleared" });
 });
 
+app.listen(8080, () => {
+  console.log("🛒 Cart service running on port 8080");
+});
