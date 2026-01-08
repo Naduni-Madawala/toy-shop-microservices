8d189be9e32957fad9b02dac130a32602c0a8d0d
Naduni Madawala <250956343+Naduni-Madawala@users.noreply.github.com>
2026-01-09 02:33:33 +0530

    Update payment.py

--- payment/payment.py
+++ payment/payment.py
@@ -1,1 +1,13 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route("/pay")
+def pay():
+    return jsonify({
+        "status": "success",
+        "message": "Toy order payment processed"
+    })
+
+app.run(host="0.0.0.0", port=8080)
 
