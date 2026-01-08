07fcabc14581aeb0f6d1ad158d2f3a4c1224bc15
Naduni Madawala <250956343+Naduni-Madawala@users.noreply.github.com>
2026-01-09 02:32:45 +0530

    Update server.js

--- cart/server.js
+++ cart/server.js
@@ -1,1 +1,20 @@
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+
+let cart = [];
+
+app.post("/cart", (req, res) => {
+  cart.push(req.body);
+  res.json({ message: "Item added to toy cart", cart });
+});
+
+app.get("/cart", (req, res) => {
+  res.json(cart);
+});
+
+app.listen(8080, () => {
+  console.log("🛒 Cart service running");
+});
 
