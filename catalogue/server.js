f7d8b3e7f05cf30d53f244eb945dfba3d028ec5e
Naduni Madawala <250956343+Naduni-Madawala@users.noreply.github.com>
2026-01-09 04:37:03 +0530

    Update server.js

--- catalogue/server.js
+++ catalogue/server.js
@@ -24,28 +24,28 @@ app.get("/seed", async (req, res) => {
 
   await Product.insertMany([
     // 🚗 Model Cars (7)
-    { name: "Ferrari F40", category: "cars", price: 4500, image: "ferrari.png" },
-    { name: "Lamborghini Aventador", category: "cars", price: 5200, image: "lamborghini.png" },
-    { name: "Bugatti Chiron", category: "cars", price: 6800, image: "bugatti.png" },
-    { name: "Porsche 911", category: "cars", price: 4800, image: "porsche.png" },
-    { name: "McLaren P1", category: "cars", price: 6100, image: "mclaren.png" },
-    { name: "BMW M4", category: "cars", price: 3900, image: "bmw.png" },
-    { name: "Audi R8", category: "cars", price: 4300, image: "audi.png" },
+    { name: "Ferrari SF-24 Diecast Model Car 2024 Formula 1", category: "cars", price: 4500, image: "ferrari.jpg" },
+    { name: "BMW i8 Modellauto Die Cast Spielzeug Model", category: "cars", price: 5200, image: "BMW.jpg" },
+    { name: "Ford Mustang Shelby GT500 White with Blue Stripes", category: "cars", price: 6800, image: "Mustang.jpg" },
+    { name: "MINIATURA CARRO F1 REDBULL MAX VERSTAPPEN RB19", category: "cars", price: 4800, image: "redbull.jpg" },
+    { name: "Jaguar - E-Type 3.8 Coupe I-Series 1961 black", category: "cars", price: 6100, image: "jaguar.jpg" },
+    { name: "Mercedes AMG Petronas F1", category: "cars", price: 6900, image: "mercedes.jpg" },
+    { name: "Classic Sculpted Model Car", category: "cars", price: 4300, image: "classic.jpg" },
 
     // 🎎 Anime Figures (13)
-    { name: "Naruto Uzumaki", category: "anime", price: 3200, image: "naruto.png" },
-    { name: "Sasuke Uchiha", category: "anime", price: 3400, image: "sasuke.png" },
-    { name: "Goku", category: "anime", price: 3600, image: "goku.png" },
-    { name: "Vegeta", category: "anime", price: 3500, image: "vegeta.png" },
-    { name: "Luffy", category: "anime", price: 3300, image: "luffy.png" },
-    { name: "Zoro", category: "anime", price: 3100, image: "zoro.png" },
-    { name: "Itachi", category: "anime", price: 3700, image: "itachi.png" },
-    { name: "Mikasa", category: "anime", price: 3000, image: "mikasa.png" },
-    { name: "Levi", category: "anime", price: 3200, image: "levi.png" },
-    { name: "Tanjiro", category: "anime", price: 3100, image: "tanjiro.png" },
-    { name: "Nezuko", category: "anime", price: 3300, image: "nezuko.png" },
-    { name: "Gojo", category: "anime", price: 3800, image: "gojo.png" },
-    { name: "Eren Yeager", category: "anime", price: 3400, image: "eren.png" }
+    { name: "Steve Harrington Funko Pop", category: "figure", price: 3200, image: "Steve.jpg" },
+    { name: "Stranger Things Pop! Nancy Wheeler with Shotgun", category: "figure", price: 3400, image: "Nancy.jpg" },
+    { name: "Funko Stranger Things Will (The Wise)", category: "figure", price: 3600, image: "Will.jpg" },
+    { name: "Stranger Things Demogorgon Pop! Vinyl Figure", category: "figure", price: 3500, image: "Demogogans.jpg" },
+    { name: "Joyce funko", category: "figure", price: 3300, image: "Joyce.jpg" },
+    { name: "Funko Pop! Stranger Things Season 5 - Eleven", category: "figure", price: 3100, image: "Elevan.jpg" },
+    { name: "mike funko pop", category: "figure", price: 3700, image: "Mike.jpg" },
+    { name: "Max mayfield season 2 funko pop", category: "figure", price: 3000, image: "Max.jpg" },
+    { name: "Funko Pop! TV: Stranger Things- Dustin", category: "figure", price: 3200, image: "Dustin.jpg" },
+    { name: "Robin season 2 funko pop", category: "figure", price: 3100, image: "Robin.jpg" },
+    { name: "Stranger Things Vecna Pop! Vinyl Figure", category: "figure", price: 3300, image: "Vecna.jpg" },
+    { name: "Hopper-Funko Pop", category: "figure", price: 3800, image: "Hopper.jpg" },
+    { name: "Funko Pop! TV: Stranger Things- Lucas", category: "figure", price: 3400, image: "Lucas.jpg" }
   ]);
 
   res.send("Toy products seeded");
