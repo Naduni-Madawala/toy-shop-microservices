5a58092767c5224618d93100a4a797c7fe3ab0be
Naduni Madawala <250956343+Naduni-Madawala@users.noreply.github.com>
2026-01-09 05:15:34 +0530

    Update Controller.java

--- shipping/src/main/java/com/naduni/toyshop/shipping/Controller.java
+++ shipping/src/main/java/com/naduni/toyshop/shipping/Controller.java
@@ -1,9 +1,18 @@
+package com.naduni.toyshop.shipping;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 @RestController
 public class Controller {
 
-    @GetMapping("/shipping")
-    public String shipping() {
-        return "Toy shipping calculated successfully";
+    @GetMapping("/health")
+    public String health() {
+        return "OK";
+    }
+
+    @GetMapping("/ship")
+    public Ship ship() {
+        return new Ship("READY", 250);
     }
 }
-
