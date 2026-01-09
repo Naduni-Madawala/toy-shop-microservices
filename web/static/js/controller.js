0725a6d0c8f2b05ffe0a871190b96026ae483272
Naduni Madawala <250956343+Naduni-Madawala@users.noreply.github.com>
2026-01-09 05:31:51 +0530

    Update controller.js

--- web/static/js/controller.js
+++ web/static/js/controller.js
@@ -21,3 +21,59 @@ app.controller("MainController", function ($scope, $http) {
 
   $scope.loadCatalogue();
 });
+app.controller("LoginController", function ($scope, $http) {
+
+  $scope.data = {
+    user: {},
+    message: "",
+    orderHistory: []
+  };
+
+  // LOGIN
+  $scope.login = function () {
+    $http.get("/api/user/users/" + $scope.data.name)
+      .then(function (response) {
+        $scope.data.user = response.data;
+        $scope.data.message = "Login successful";
+        $scope.loadOrders();
+      }, function () {
+        $scope.data.message = "Invalid user";
+      });
+  };
+
+  // REGISTER
+  $scope.register = function () {
+    if ($scope.data.password !== $scope.data.password2) {
+      $scope.data.message = "Passwords do not match";
+      return;
+    }
+
+    const user = {
+      username: $scope.data.name,
+      name: $scope.data.name,
+      email: $scope.data.email
+    };
+
+    $http.post("/api/user/users", user)
+      .then(function (response) {
+        $scope.data.user = response.data;
+        $scope.data.message = "Registration successful";
+      });
+  };
+
+  // MOCK ORDER HISTORY (Robot-Shop style demo)
+  $scope.loadOrders = function () {
+    $scope.data.orderHistory = [
+      {
+        orderid: "TS-1001",
+        cart: {
+          items: [
+            { name: "Ferrari F40" },
+            { name: "Naruto Figure" }
+          ],
+          total: 7800
+        }
+      }
+    ];
+  };
+});
