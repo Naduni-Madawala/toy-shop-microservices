923793d5ad92565d64c733371d70362765f274b9
Naduni Madawala <250956343+Naduni-Madawala@users.noreply.github.com>
2026-01-09 05:33:12 +0530

    Update controller.js

--- web/static/js/controller.js
+++ web/static/js/controller.js
@@ -2,25 +2,43 @@ var app = angular.module("ToyShopApp", []);
 
 app.controller("MainController", function ($scope, $http) {
 
-  $scope.products = [];
-  $scope.cart = [];
+  $scope.data = {
+    products: [],
+    cart: { items: [], total: 0 },
+    user: {},
+    message: "",
+    orderHistory: []
+  };
 
+  /* ---------------- CATALOGUE ---------------- */
   $scope.loadCatalogue = function () {
     $http.get("/api/catalogue")
-      .then(function (response) {
-        $scope.products = response.data;
-      });
+      .then(res => $scope.data.products = res.data);
   };
 
+  /* ---------------- CART ---------------- */
   $scope.addToCart = function (product) {
-    $http.post("/api/cart", product)
-      .then(function () {
-        alert("Added to cart");
-      });
+    $scope.data.cart.items.push(product);
+    $scope.data.cart.total += product.price;
   };
 
+  /* ---------------- PAYMENT ---------------- */
+  $scope.pay = function () {
+    $http.post("/api/payment/pay", $scope.data.cart)
+      .then(() => alert("Payment successful"));
+  };
+
+  /* ---------------- SHIPPING ---------------- */
+  $scope.getShipping = function () {
+    $http.get("/api/shipping/ship")
+      .then(res => $scope.data.shipping = res.data);
+  };
+
+  /* ---------------- INIT ---------------- */
   $scope.loadCatalogue();
 });
+
+/* ---------------- LOGIN CONTROLLER ---------------- */
 app.controller("LoginController", function ($scope, $http) {
 
   $scope.data = {
@@ -29,51 +47,38 @@ app.controller("LoginController", function ($scope, $http) {
     orderHistory: []
   };
 
-  // LOGIN
   $scope.login = function () {
     $http.get("/api/user/users/" + $scope.data.name)
-      .then(function (response) {
-        $scope.data.user = response.data;
+      .then(res => {
+        $scope.data.user = res.data;
         $scope.data.message = "Login successful";
         $scope.loadOrders();
-      }, function () {
-        $scope.data.message = "Invalid user";
-      });
+      }, () => $scope.data.message = "Invalid user");
   };
 
-  // REGISTER
   $scope.register = function () {
     if ($scope.data.password !== $scope.data.password2) {
       $scope.data.message = "Passwords do not match";
       return;
     }
 
-    const user = {
+    $http.post("/api/user/users", {
       username: $scope.data.name,
       name: $scope.data.name,
       email: $scope.data.email
-    };
-
-    $http.post("/api/user/users", user)
-      .then(function (response) {
-        $scope.data.user = response.data;
-        $scope.data.message = "Registration successful";
-      });
+    }).then(res => {
+      $scope.data.user = res.data;
+      $scope.data.message = "Registration successful";
+    });
   };
 
-  // MOCK ORDER HISTORY (Robot-Shop style demo)
   $scope.loadOrders = function () {
-    $scope.data.orderHistory = [
-      {
-        orderid: "TS-1001",
-        cart: {
-          items: [
-            { name: "Ferrari F40" },
-            { name: "Naruto Figure" }
-          ],
-          total: 7800
-        }
+    $scope.data.orderHistory = [{
+      orderid: "TS-1001",
+      cart: {
+        items: [{ name: "Ferrari SF-24 Diecast Model Car 2024 Formula 1" }, { name: "Steve Harrington Funko Pop" }],
+        total: 7800
       }
-    ];
+    }];
   };
 });
